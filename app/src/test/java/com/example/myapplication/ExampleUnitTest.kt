bb3b528fa734dc321f66afa1989b4ef0f7a2c653
Burak <70605238+SoofiBD@users.noreply.github.com>
2023-07-25 14:46:09 +0300

    added simple UnitTest

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -11,7 +11,10 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = MainActivity.Dice(6)
+        val diceRoll = dice.roll()
+        assertTrue(diceRoll in 1..6)
+
     }
 }
