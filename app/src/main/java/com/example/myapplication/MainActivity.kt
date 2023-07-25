969617aa273c2266491f49367dd6d2010c34ae17
Burak <70605238+SoofiBD@users.noreply.github.com>
2023-07-25 14:03:13 +0300

    dice images added when instead of if/else

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
+@Suppress("UNUSED_EXPRESSION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,44 +14,37 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+
+        rollDice()
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
         val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-
-        if (diceRoll == 1) {
-            val toast =
-                Toast.makeText(this, "You rolled a 1! Great job!", Toast.LENGTH_SHORT).show()
-        } else if (diceRoll == 2) {
-            val toast =
-                Toast.makeText(this, "You rolled a 2! Great job!", Toast.LENGTH_SHORT).show()
-        } else if (diceRoll == 3) {
-            val toast =
-                Toast.makeText(this, "You rolled a 3! Great job!", Toast.LENGTH_SHORT).show()
-        } else if (diceRoll == 4) {
-            val toast =
-                Toast.makeText(this, "You rolled a 4! Great job!", Toast.LENGTH_SHORT).show()
-        } else if (diceRoll == 5) {
-            val toast =
-                Toast.makeText(this, "You rolled a 5! Great job!", Toast.LENGTH_SHORT).show()
-        } else if (diceRoll == 6) {
-            val toast =
-                Toast.makeText(this, "You rolled a 6! Great job!", Toast.LENGTH_SHORT).show()
+        toast // This is to remove the warning of unused variable
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else ->  R.drawable.dice_6
         }
+
+        diceImage.setImageResource(drawableResource)
+
+        diceImage.contentDescription = diceRoll.toString()
     }
-}
 
 class Dice(val numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
     }
+}
+
 }
